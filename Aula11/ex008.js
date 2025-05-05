8126399511d8e75985ae0cf2dd7b1e516a03734c
Giovani Silva Rodrigues <giovani25900@gmail.com>
2025-05-05 17:10:01 -0300

    Meu primeiro commit

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(vel)
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+else(vel <= 60)
+    console.log('Dirija sempre com segurança!')
